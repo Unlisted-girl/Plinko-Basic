588133d7a782ed171e6de2d875cdb46cc7f2f445
Unlisted-girl <seerat.kaur@gmail.com>
2020-06-27 09:58:18 +0530

    Code for maam

--- sketch.js
+++ sketch.js
@@ -1,9 +1,32 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground;
+var particles = [];
+var plinkos = [];
+var divisions = [];
+var divisionHeight = 100;
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(480,800);
+  //createSprite(400, 200, 50, 50);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(240, 790, 480, 10);
+
+  
 }
 
 function draw() {
-  background(255,255,255);  
+  background("black");  
   drawSprites();
+  Engine.update(engine);
+
+  ground.display();
+  
+  
 }
