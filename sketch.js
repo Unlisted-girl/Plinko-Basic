8c5f8185af6494bdd32d09064c6422b2e09b80c5
Unlisted-girl <seerat.kaur@gmail.com>
2020-07-02 17:45:51 +0530

    Plinko game finally completed!

--- sketch.js
+++ sketch.js
@@ -3,11 +3,11 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var ground;
+var ground, loop;
 var particles = [];
 var plinkos = [];
 var divisions = [];
-var divisionHeight = 100;
+var divisionHeight = 300;
 
 function setup() {
   createCanvas(480,800);
@@ -16,8 +16,30 @@ function setup() {
   engine = Engine.create();
   world = engine.world;
 
-  ground = new Ground(240, 790, 480, 10);
+  ground = new Ground(240, 800, 480, 10);
 
+  for (var k = 0 ; k<= width; k = k+80){
+     divisions.push(new Division(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+
+  for(var j = 40; j<=width; j = j+50){
+    plinkos.push(new Plinko(j,75));
+  }
+
+  for(var j = 15; j<=width; j = j+50){
+    plinkos.push(new Plinko(j,175));
+  }
+
+  for(var j = 40; j<=width; j = j+50){
+    plinkos.push(new Plinko(j,275));
+  }
+
+  for(var j = 15; j<=width; j = j+50){
+    plinkos.push(new Plinko(j,375));
+  }
+  
+
+  fallParticle();
   
 }
 
@@ -27,6 +49,22 @@ function draw() {
   Engine.update(engine);
 
   ground.display();
+
+  for(var j = 0; j<particles.length; j++){
+    particles[j].display();
+  }
+  for (var k = 0; k< divisions.length; k++){
+    divisions[k].display();
+  }
+
+  for(var j = 0; j < plinkos.length; j++){
+    plinkos[j].display();
+  }
   
-  
+}
+
+function fallParticle(){
+  if(frameCount%60 === 0){
+    particles.push(new Particle(random(width/2-10,width/2+10),10,10));
+  }
 }
